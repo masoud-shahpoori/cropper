d46df365b8ae78aaa1aa67d284d10460d7236654
m.shahpoori <masoudshahpoori97@gmail.com>
2023-07-04 10:03:05 +0330

    first commit

---
+++ src/cropper/cropper.container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CropperComponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default CropperComponent;
+
+
+
+/*Make resizable div by Hung Nguyen*/
---
+++ src/cropper/resize.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ResizeComponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ResizeComponent;
